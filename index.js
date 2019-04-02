7af684b60e628fa959fabc710e1ce41ed0a13c3a
Eric Mauldin <emauldin84@gmail.com>
2019-04-02 08:24:03 -0400

    requires axios to make an HTTP request to yerkee.com for a new, random fortune

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const app = express();
 const port = 3000;
 
 const Fortune = require('./models/fortune')
-console.log(Fortune.randomJoke(Fortune.fortunes))
+// console.log(Fortune.randomJoke(Fortune.fortunes))
+
+const axios = require('axios');
+axios.get('http://yerkee.com/api/fortune')
+    .then(res => {
+        console.log(res.data.fortune);
+        joke = res.data.fortune;
+    })
+    .catch(error => {
+        console.log(error);
+    });
 
 app.get('/fortune', (req, res) => {
-    res.send(`${Fortune.randomJoke(Fortune.fortunes)}`)
+    res.send(`${joke}`)
     
 })
 

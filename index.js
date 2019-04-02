68d65343141b9c6a9c374690129919b96d699c2b
Eric Mauldin <emauldin84@gmail.com>
2019-04-02 09:02:08 -0400

    displays new fortune on page reload

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const Fortune = require('./models/fortune')
+const Fortune = require('./models/fortune');
+const jokeUrl = 'http://yerkee.com/api/fortune';
 // console.log(Fortune.randomJoke(Fortune.fortunes))
 
-const axios = require('axios');
-axios.get('http://yerkee.com/api/fortune')
-    .then(res => {
-        console.log(res.data.fortune);
-        joke = res.data.fortune;
-    })
-    .catch(error => {
-        console.log(error);
-    });
 
 app.get('/fortune', (req, res) => {
-    res.send(`${joke}`)
-    
-})
+    const axios = require('axios');
+    axios.get(jokeUrl)
+        .then(response => {
+            console.log(response.data.fortune);
+            joke = response.data.fortune;
+            res.send(`${joke}`)
+        })
+        .catch(error => {
+            console.log(error);
+        });    
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
